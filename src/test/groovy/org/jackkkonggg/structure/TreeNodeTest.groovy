f44bb6fdd6e1091f615ef9b7aa10985a69d53ade
jack.ong <jack.ong@okg.com>
2023-07-10 12:18:09 +0800

    test: additional test cases for TreeNode.fromList

--- src/test/groovy/org/jackkkonggg/structure/TreeNodeTest.groovy
+++ src/test/groovy/org/jackkkonggg/structure/TreeNodeTest.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 class TreeNodeTest extends Specification {
     def "fromList should work with [3,9,20,null,null,15,7]"() {
         given:
-        List<Integer> preorder = [3,9,20,null,null,15,7]
+        List<Integer> preorder = [3, 9, 20, null, null, 15, 7]
         TreeNode root = TreeNode.fromList(preorder)
 
         expect:
@@ -16,5 +16,18 @@ class TreeNodeTest extends Specification {
         root.left.right == null
         root.right.left.val == 15
         root.right.right.val == 7
+    }
+
+    def "fromList should work with [2,null,3,null,4,null,5,null,6]"() {
+        given:
+        List<Integer> preorder = [2, null, 3, null, 4, null, 5, null, 6]
+        TreeNode root = TreeNode.fromList(preorder)
+
+        expect:
+        root.val == 2
+        root.right.val == 3
+        root.right.right.val == 4
+        root.right.right.right.val == 5
+        root.right.right.right.right.val == 6
     }
 }
