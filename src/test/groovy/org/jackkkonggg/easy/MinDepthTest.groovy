182bb3724a98d08275a9c918da5067b0faaa61e1
jack.ong <jack.ong@okg.com>
2023-07-10 10:57:08 +0800

    test: init min depth test class

---
+++ src/test/groovy/org/jackkkonggg/easy/MinDepthTest.groovy
@@ -0,0 +1,11 @@
+package org.jackkkonggg.easy
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class MinDepthTest extends Specification {
+    @Shared
+    def minDepth = new MinDepth()
+
+
+}
