01709efbbd777a96ea8d5e4db376b9cf0c028368
jack.ong <jack.ong@okg.com>
2023-07-10 12:16:16 +0800

    feat: TreeNode.fromList method

--- src/main/java/org/jackkkonggg/structure/TreeNode.java
+++ src/main/java/org/jackkkonggg/structure/TreeNode.java
@@ -1,6 +1,11 @@
 package org.jackkkonggg.structure;
 
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+
 public class TreeNode {
     int val;
     public TreeNode left;
@@ -17,5 +22,43 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    // Sample Input: [3,9,20,null,null,15,7]
+    public static TreeNode fromList(List<Integer> preorder) {
+        if (preorder.size() == 0) return null;
+
+        TreeNode root = new TreeNode(preorder.get(0));
+        Queue<TreeNode> queue = new ArrayDeque<>(Collections.singletonList(root));
+
+        Branch side = Branch.Left;
+        for (int i = 1; i < preorder.size(); ++i) {
+            TreeNode front = queue.peek();
+            if (front == null) throw new IllegalStateException();
+
+            Integer val = preorder.get(i);
+            if (side == Branch.Left) {
+                if (val != null) {
+                    front.left = new TreeNode(val);
+                    queue.add(front.left);
+                }
+                side = Branch.Right;
+            } else {
+                if (val != null) {
+                    front.right = new TreeNode(val);
+                    queue.add(front.right);
+                }
+
+                queue.poll();
+                side = Branch.Left;
+            }
+        }
+
+        return root;
+    }
+
+    private enum Branch {
+        Left,
+        Right
     }
 }
