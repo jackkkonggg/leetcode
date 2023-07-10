32fab595db76ebbf4fad112a8370eba08e96f88c
jack.ong <jack.ong@okg.com>
2023-07-10 12:21:56 +0800

    test: example tests for min depth class

--- src/test/groovy/org/jackkkonggg/easy/MinDepthTest.groovy
+++ src/test/groovy/org/jackkkonggg/easy/MinDepthTest.groovy
@@ -1,5 +1,6 @@
 package org.jackkkonggg.easy
 
+import org.jackkkonggg.structure.TreeNode
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -7,5 +8,14 @@ class MinDepthTest extends Specification {
     @Shared
     def minDepth = new MinDepth()
 
+    def "example tests"() {
+        expect:
+        depth == minDepth.solve(root)
+
+        where:
+        root                                                       | depth
+        TreeNode.fromList([3, 9, 20, null, null, 15, 7])           | 2
+        TreeNode.fromList([2, null, 3, null, 4, null, 5, null, 6]) | 5
+    }
 
 }
