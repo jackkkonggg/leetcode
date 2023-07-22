beb355b9d2999d2013b1bcf978c283b06178485d
Jack Ong <jqong@connect.ust.hk>
2023-07-22 13:57:52 +0800

    feat: init knight probability in chessboard

---
+++ src/main/java/org/jackkkonggg/medium/KnightProbabilityInChessBoard.java
@@ -0,0 +1,14 @@
+package org.jackkkonggg.medium;
+
+public class KnightProbabilityInChessBoard {
+    /**
+     * @param n n by n chessboard
+     * @param k number of moves
+     * @param row y-position
+     * @param column x-position
+     * @return  probability that the knight remains on the board after it has stopped moving
+     */
+    public double knightProbability(int n, int k, int row, int column) {
+        return 0;
+    }
+}
